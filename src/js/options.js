c0376248c84a2b1cee6cc226e382da61c00b77f5
acgotaku <acgotaku311@gmail.com>
2019-05-11 15:03:00 +0900

    add i18n support

--- src/js/options.js
+++ src/js/options.js
@@ -14,7 +14,24 @@ const vm = new Vue({
         path: 'http://localhost:6800/jsonrpc'
       }],
       whitelist: '',
-      blocklist: ''
+      blocklist: '',
+      saved: false,
+      title: chrome.i18n.getMessage('title'),
+      contextMenu: chrome.i18n.getMessage('contextMenu'),
+      contextMenuDesc: chrome.i18n.getMessage('contextMenuDesc'),
+      syncConfig: chrome.i18n.getMessage('syncConfig'),
+      syncConfigDesc: chrome.i18n.getMessage('syncConfigDesc'),
+      interception: chrome.i18n.getMessage('interception'),
+      interceptionDesc: chrome.i18n.getMessage('interceptionDesc'),
+      fileSize: chrome.i18n.getMessage('fileSize'),
+      unit: chrome.i18n.getMessage('unit'),
+      addRPC: chrome.i18n.getMessage('addRPC'),
+      removeRPC: chrome.i18n.getMessage('removeRPC'),
+      whitelistStr: chrome.i18n.getMessage('whitelistStr'),
+      blocklistStr: chrome.i18n.getMessage('blocklistStr'),
+      save: chrome.i18n.getMessage('save'),
+      saveSuccess: chrome.i18n.getMessage('saveSuccess'),
+      reset: chrome.i18n.getMessage('reset')
     }
   },
   mounted () {
@@ -39,11 +56,19 @@ const vm = new Vue({
         path: ''
       })
     },
-    removeRPC (index) {
+    removeRPCByIndex (index) {
       this.rpcLists.splice(index, 1)
     },
-    save () {
-      const configData = this.$data
+    saveConfig () {
+      const configData = {
+        isContextMenus: this.isContextMenus,
+        isInterception: this.isInterception,
+        isSync: this.isSync,
+        fileSize: this.fileSize,
+        rpcLists: this.rpcLists,
+        whitelist: this.whitelist,
+        blocklist: this.blocklist
+      }
       for (let key in configData) {
         chrome.storage.local.set({ [key]: configData[key] }, () => {
           console.log('chrome local set: %s, %s', key, configData[key])
@@ -62,96 +87,12 @@ const vm = new Vue({
       location.reload()
     },
     showSavedInfo () {
-      const info = document.querySelector('.config-saved')
-      info.classList.add('show')
+      this.saved = true
       setTimeout(() => {
-        info.classList.remove('show')
+        this.saved = false
       }, 3000)
     }
   }
 })
 
 vm.$mount('#app')
-
-// $(function () {
-//   var config = (function () {
-//     return {
-//       init: function () {
-//         var self = this
-//         var contextMenus = localStorage.getItem('contextMenus')
-//         if (contextMenus == 'true') {
-//           $('#contextMenus').prop('checked', true)
-//         }
-//         var integration = localStorage.getItem('integration')
-//         if (integration == 'true') {
-//           $('#integration').prop('checked', true)
-//         }
-//         var fileSize = localStorage.getItem('fileSize') || 500
-//         $('#fileSize').val(fileSize)
-//         var rpc_list = JSON.parse(localStorage.getItem('rpc_list') || '[{"name":"ARIA2 RPC","url":"http://localhost:6800/jsonrpc"}]')
-//         for (var i in rpc_list) {
-//           var addBtn = i == 0 ? '<button class="btn" id="add-rpc">Add RPC</button>' : ''
-//           var row = '<div class="control-group rpc_list"><label class="control-label">JSON-RPC</label><div class="controls"><input type="text" class="input-small" value="' + rpc_list[i]['name'] + '" placeholder="RPC Name"><input type="text" class="input-xlarge rpc-path" value="' + rpc_list[i]['url'] + '" placeholder="RPC Path">' + addBtn + '</div></div>'
-//           if ($('.rpc_list').length > 0) {
-//             $(row).insertAfter($('.rpc_list').eq(i - 1))
-//           } else {
-//             $(row).insertAfter($('fieldset').children().eq(2))
-//           }
-//         }
-//         var black_site = JSON.parse(localStorage.getItem('black_site'))
-//         if (black_site) {
-//           $('#black-site').val(black_site.join('\n'))
-//         }
-//         var white_site = JSON.parse(localStorage.getItem('white_site'))
-//         if (white_site) {
-//           $('#white-site').val(white_site.join('\n'))
-//         }
-//         $('#add-rpc').on('click', function () {
-//           var rpc_form = '<div class="control-group rpc_list">' +
-//                         '<label class="control-label">JSON-RPC</label>' +
-//                         '<div class="controls">' +
-//                           '<input type="text" class="input-small"  placeholder="RPC Name">' +
-//                           '<input type="text" class="input-xlarge rpc-path"  placeholder="RPC Path"></div></div>'
-//           $(rpc_form).insertAfter($('.rpc_list')[0])
-//         })
-//         $('#save').on('click', function () {
-//           self.save()
-//         })
-//         $('#reset').on('click', function () {
-//           localStorage.clear()
-//           location.reload()
-//         })
-//       },
-//       save: function () {
-//         var rpc_list = []
-//         var jsonrpc_history = []
-//         for (var i = 0; i < $('.rpc_list').length; i++) {
-//           var child = $('.rpc_list').eq(i).children().eq(1).children()
-//           if (child.eq(0).val() != '' && child.eq(1).val() != '') {
-//             rpc_list.push({ 'name': child.eq(0).val(), 'url': child.eq(1).val() })
-//             jsonrpc_history.push(child.eq(1).val())
-//           }
-//         }
-//         localStorage.setItem('rpc_list', JSON.stringify(rpc_list))
-//         localStorage.setItem('jsonrpc_history', JSON.stringify(jsonrpc_history))
-//         if ($('#contextMenus').prop('checked') == true) {
-//           localStorage.setItem('contextMenus', true)
-//         } else {
-//           localStorage.setItem('contextMenus', false)
-//         }
-//         if ($('#integration').prop('checked') == true) {
-//           localStorage.setItem('integration', true)
-//         } else {
-//           localStorage.setItem('integration', false)
-//         }
-//         var fileSize = $('#fileSize').val()
-//         localStorage.setItem('fileSize', fileSize)
-//         var black_site = $('#black-site').val().split('\n')
-//         localStorage.setItem('black_site', JSON.stringify(black_site))
-//         var white_site = $('#white-site').val().split('\n')
-//         localStorage.setItem('white_site', JSON.stringify(white_site))
-//       }
-//     }
-//   })()
-//   config.init()
-// })
